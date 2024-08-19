7b59aa70d881919575bc998ceb35b95c670c1b51
Boqiang Xu <41889015+xbq1994@users.noreply.github.com>
2024-08-19 13:58:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-from flask import Flask, jsonify
-import os
+from flask import Flask, request, render_template_string
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+    user_agent = request.headers.get('User-Agent')
 
+    if 'Mobile' in user_agent:
+        return render_template_string("<h1>Mobile Version</h1><p>You are using a mobile device.</p>")
+    elif 'Chrome' in user_agent:
+        return render_template_string("<h1>Chrome Browser</h1><p>You are using Chrome browser.</p>")
+    else:
+        return render_template_string("<h1>Desktop Version</h1><p>You are using a desktop device.</p>")
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True)
