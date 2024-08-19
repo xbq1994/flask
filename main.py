2893e1e6af0a4a6c83a5192855d5a5eda87f07be
Boqiang Xu <41889015+xbq1994@users.noreply.github.com>
2024-08-19 16:29:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from flask import Flask, request, render_template_string, render_template
+import logging
 
 app = Flask(__name__, template_folder="templates")
+logging.basicConfig(level=logging.INFO)
 
 @app.route('/')
 def index():
+    app.logger.info(f'Accessed: {request.path} from {request.remote_addr}')
     user_agent = request.headers.get('User-Agent')
-
     if 'ChatGPT' in user_agent:  # now add condition on LLM agent specified string and render accordingly
         return render_template("alex_fake.html")
     elif 'Chrome' in user_agent:
