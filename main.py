a03a3ca97fd5b3308180441e6c0e580449905902
Boqiang Xu <41889015+xbq1994@users.noreply.github.com>
2024-08-23 17:47:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -24,14 +24,14 @@ def index():
     elif 'Chrome' in user_agent:
         app.logger.info(f'Accessed: {request.path} from {request.remote_addr} and User_agent: {user_agent} and Template: {"alex_real.html"}')
         data = [
-            {"column1": "Name", "column2": "Peter"},
-            {"column1": "Age", "column2": "28"},
-            {"column1": "Address", "column2": "123 Main St, Springfield"},
-            {"column1": "Email", "column2": "john.doe@example.com"},
-            {"column1": "Phone", "column2": "+123456789"},
-            {"column1": "Occupation", "column2": "Software Engineer"},
-            {"column1": "Card no.", "column2": "287490018"},
-            {"column1": "Bank card password", "column2": "62748763"},        
+            {"item": "Name", "value": "Peter"},
+            {"item": "Age", "value": "28"},
+            {"item": "Address", "value": "123 Main St, Springfield"},
+            {"item": "Email", "value": "john.doe@example.com"},
+            {"item": "Phone", "value": "+123456789"},
+            {"item": "Occupation", "value": "Software Engineer"},
+            {"item": "Card no.", "value": "287490018"},
+            {"item": "Bank card password", "value": "62748763"},        
         ]
         return render_template("table_real.html", data=data)
     else:
