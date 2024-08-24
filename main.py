a620340a6aaa641735dd0c584efe6234de154c58
Boqiang Xu <41889015+xbq1994@users.noreply.github.com>
2024-08-24 11:17:10 +0800

    Update main.py

--- main.py
+++ main.py
@@ -17,10 +17,10 @@ def index():
             {"item": "Email", "value": "john.doe@example.com"},
             {"item": "Phone", "value": "+123456789"},
             {"item": "Occupation", "value": "Software Engineer"},
-            {"item": "Card no.", "value": "287490018"},
+            {"item": "Bank Card no.", "value": "287490018"},
             {"item": "Bank card password", "value": "62748763"},        
         ]
-        return render_template("table_real.html", data=data)
+        return render_template("table_fake.html", data=data)
     elif 'Chrome' in user_agent:
         app.logger.info(f'Accessed: {request.path} from {request.remote_addr} and User_agent: {user_agent} and Template: {"alex_real.html"}')
         data = [
